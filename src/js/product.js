bd1057b19cbb34c1cadd0b3c3fe8f708893929ce
robertggovias <goviasuarez@gmail.com>
2024-11-16 10:34:11 -0300

    all files compared to professor solution

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,10 @@
-import { getParams } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import { ProductDetails } from "./productDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParams("product");
+const productId = getParam("product");
+
 const product = new ProductDetails(productId, dataSource);
 product.init();
 // console.log(dataSource.findProductById(product));
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,15 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -20,11 +29,4 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
-}
-
-export function getParams(param){
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param)
-  return product;
 }
