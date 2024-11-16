f1e7894b6172e5aabf58d48ff5113966f58bc3f6
robertggovias <goviasuarez@gmail.com>
2024-11-16 13:17:00 -0300

    falta crear el html

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import { ProductData } from './ProductData.mjs';
+import {ProductListing} from './ProductList.mjs';
+
+const productData = new ProductData();
+
+const productListing = new ProductListing()
+productListing.listElement
+
