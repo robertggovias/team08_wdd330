6c72329c527e5455b571fa979d4af207b8f2f657
robertggovias <goviasuarez@gmail.com>
2024-11-24 16:03:08 -0300

    productlistin.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,4 +1,4 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getParams, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import { ProductListing } from "./productList.mjs";
 import { cartSuperScript } from "./cartSuperscript.mjs";
@@ -9,14 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter().then(() => {
     // Instantiates the product data class and store
     // creates a data source.
-    const dataSource = new ProductData("tents");
+    const category = getParams('caregory')
+    const dataSource = new ProductData();
 
     // Get an element on the DOM.
     const listElement = document.querySelector(".product-list");
 
     // Create an instance of ProductListing class and pass in the
     // product category, data source to get data and the element where products are listed.
-    const productsListing = new ProductListing("Tents", dataSource, listElement);
+    const productsListing = new ProductListing(category, dataSource, listElement);
 
     // Initialize the function.
     productsListing.init();
