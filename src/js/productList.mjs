c40da3128fb9ef6f350da372f4d9046b22eea67d
robertggovias <goviasuarez@gmail.com>
2024-11-24 22:39:39 -0300

    discount flag

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -8,7 +8,7 @@ function productCartTemplate(product){
             <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
             <h3 class="card__brand">${product.Brand.Name}</h3>
             <h2 class="card__name">${product.Name}</h2>
-            <p class="product-card__price">$${product.FinalPrice}  <span class="discount"> $${discount.toFixed(2)} off</span>% ${discountFlag} <span class="discount_flag">
+            <p class="product-card__price">$${product.FinalPrice}  <span class="discount"> save $${discount.toFixed(2)} </span> - % ${discountFlag} discount!<span class="discount_flag">
             </p>
           </a>
         </li>`;
